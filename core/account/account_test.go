1a6c875b069bc4c87b75f4dadd4e2d9ee3dce16a
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-05 02:30:48 +0700

    Account service register test

--- core/account/account_test.go
+++ core/account/account_test.go
@@ -1,13 +1,33 @@
-package account
+package account_test
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/supendi/orderan.api/core/account"
+	"github.com/supendi/orderan.api/core/account/inmem"
+)
 
 func TestRegisterAccount(t *testing.T) {
-	// registrant := &Registrant{
-	// 	Name:     "joe",
-	// 	Email:    "joe@gmail.com",
-	// 	Phone:    "0813",
-	// 	Password: "",
-	// }
-	// var
+	registrant := &account.Registrant{
+		Name:     "joe",
+		Email:    "joe@gmail.com",
+		Phone:    "0813",
+		Password: "",
+	}
+	accountRepo := inmem.NewAccountRepository([]*account.Account{})
+	hasher := account.NewBCryptHasher()
+	accountSvc := account.NewAccountService(accountRepo, hasher)
+	newContext := context.Background()
+	newAccount, err := accountSvc.RegisterAccount(newContext, registrant)
+	if err != nil {
+		t.Fatal(err)
+	}
+	accountRecord, err := accountRepo.GetByEmail(newContext, newAccount.Email)
+	if accountRecord == nil {
+		t.Fatal("New account should be not nil")
+	}
+	if accountRecord.ID == "" {
+		t.Fatal("New account ID should be not empty")
+	}
 }
