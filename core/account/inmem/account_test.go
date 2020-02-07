8880f029a61679bf4bfa630c2c9f5dfa3dfff0ab
Irpan Supendi <irpan.supendi@gmail.com>
2020-02-07 09:34:55 +0700

    update account inmem repo test

--- core/account/inmem/account_test.go
+++ core/account/inmem/account_test.go
@@ -93,3 +93,59 @@ func TestGetAccountByPhone(t *testing.T) {
 	}
 	t.Log(got.ID)
 }
+func TestGetAccountByID(t *testing.T) {
+	registrant := &account.Account{
+		Name:     "Lady",
+		Email:    "lady.gaga@gmail.com",
+		Phone:    "+1234",
+		Password: "strongPassw04rd",
+	}
+	newContext := context.Background()
+	repo := NewAccountRepository([]*account.Account{})
+	newAccount, err := repo.Add(newContext, registrant)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := repo.GetByID(newContext, newAccount.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got == nil {
+		t.Fatal("Retrieved account must be not nil")
+	}
+	if got.ID != newAccount.ID {
+		t.Fatal("Wrong ID")
+	}
+	t.Log(got.ID)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	registrant := &account.Account{
+		Name:     "Lady",
+		Email:    "lady.gaga@gmail.com",
+		Phone:    "+1234",
+		Password: "strongPassw04rd",
+	}
+	newContext := context.Background()
+	repo := NewAccountRepository([]*account.Account{})
+	newAccount, err := repo.Add(newContext, registrant)
+	if err != nil {
+		t.Fatal(err)
+	}
+	existingAccount, err := repo.GetByID(newContext, newAccount.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if existingAccount == nil {
+		t.Fatal("Retrieved account must be not nil")
+	}
+	existingAccount.Name = "newName"
+	updatedAccount, err := repo.Update(newContext, existingAccount)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if updatedAccount.Name != "newName" {
+		t.Fatal("Wrong Name")
+	}
+}
